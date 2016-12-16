1be3d568e987d9b7c67965c7d63de3bb1b7212d4
Fatih Türker <fatihturker35@gmail.com>
2016-12-17 02:25:00 +0300

    first commit

---
+++ Lucia/Controllers/ViewBoosterController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lucia.Controllers
+{
+    public class ViewBoosterController : ApiController
+    {
+        [AcceptVerbs("GET")]
+        public void Boost(string id)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://www.youtube.com/watch?v=" + id);
+
+            request.Proxy = new WebProxy("127.0.0.1:8118", true);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+        }
+    }
+}
---
+++ Lucia/Utility/ProxyProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lucia.Utility
+{
+    public class ProxyProvider
+    {
+        public List<string> GetProxyList()
+        {
+            return null;
+        }
+    }
+}
